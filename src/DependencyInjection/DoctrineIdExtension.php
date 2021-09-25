7cf7dbc76459ae87be90802a86500c549c82dbe0
Pavel Chaplygin <mail@pchapl.dev>
2021-09-25 12:34:11 +0300

    Move config loading to compile pass

--- src/DependencyInjection/DoctrineIdExtension.php
+++ src/DependencyInjection/DoctrineIdExtension.php
@@ -2,6 +2,7 @@
 
 namespace PChapl\DoctrineIdBundle\DependencyInjection;
 
+use PChapl\DoctrineIdBundle\Exception\InvalidConfigurationException;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
@@ -9,5 +10,30 @@ final class DoctrineIdExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
+        $config = $this->processConfiguration(new Configuration(), $configs);
+
+        $idTypes = $config['types'] ?? [];
+
+        $types = $container->hasParameter('doctrine.dbal.connection_factory.types')
+            ? $container->getParameter('doctrine.dbal.connection_factory.types')
+            : [];
+
+        foreach ($idTypes as $typeName => $class) {
+            if (!class_exists($class)) {
+                throw new InvalidConfigurationException("Can not load class $class");
+            }
+            if (array_key_exists($typeName, $types)) {
+                throw new InvalidConfigurationException(
+                    sprintf(
+                        'Duplicate key "%s": type should be described either for %s or %s',
+                        $typeName,
+                        'parameters.doctrine_id.types',
+                        'doctrine.dbal.types',
+                    )
+                );
+            }
+        }
+
+        $container->setParameter('pchapl.doctrine_id.types', $idTypes);
     }
 }
